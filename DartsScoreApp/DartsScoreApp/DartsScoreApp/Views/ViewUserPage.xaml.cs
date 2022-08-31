7631a85d7e851a7e91d508201dd4cad0737547bf
Jack D <jackdenman2000@gmail.com>
2022-08-31 21:06:07 +0100

    Changed user view to use binding context correctly

--- DartsScoreApp/DartsScoreApp/DartsScoreApp/Views/ViewUserPage.xaml.cs
+++ DartsScoreApp/DartsScoreApp/DartsScoreApp/Views/ViewUserPage.xaml.cs
@@ -13,9 +13,16 @@ namespace DartsScoreApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     // Passes in user id from GoToAsync call
-    //[QueryProperty(nameof(UserId), nameof(UserId))]
+    [QueryProperty(nameof(Id), nameof(Id))]
     public partial class ViewUserPage : ContentPage
     {
+        public new string Id
+        {
+            set
+            {
+                LoadUser(int.Parse(value));
+            }
+        }
         // Sets page's binding context to the selected user
         /*public string UserId
         {
@@ -26,16 +33,17 @@ namespace DartsScoreApp.Views
         }*/
         //public string UserName { get; set; }
 
+        // Sets the binding context of the page to a specific user
         public ViewUserPage(User user)
         {
             InitializeComponent();
 
             BindingContext = user;
-            username.Text = user.Username;
-            Title = user.Username;
+            //username.Text = user.Username;
+            //Title = user.Username;
         }
 
-        /*async void LoadUser(int id)
+        async void LoadUser(int id)
         {
             try
             {
@@ -46,6 +54,6 @@ namespace DartsScoreApp.Views
             {
                 Console.WriteLine("Failed to load user:\n" + ex.Message);
             }
-        }*/
+        }
     }
 }
